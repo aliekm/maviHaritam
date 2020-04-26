c20366f74d5e645328240bf56ccd1c618b50d6cb
aliekm <51740366+aliekm@users.noreply.github.com>
2020-04-26 17:36:23 +0300

    Update mavijava.js

--- mavijava.js
+++ mavijava.js
@@ -136,7 +136,9 @@ function setShareInfoButton() {
   let shareInfoBtn = document.getElementById("shareInfoID").appendChild(shareInfoButton);
   shareInfoBtn.className = "shareInfo"
   shareInfoButton.addEventListener('click', () => {
-    window.location.href = "mailto://maviharitam@gmail.com?subject=İşletme hakkında bilgi?target=_blank"; } ); 
+    window.location.target = '_blank';
+    window.location.href = "mailto://maviharitam@gmail.com?subject=İşletme hakkında bilgi"; 
+  } );  
 }
 
 
