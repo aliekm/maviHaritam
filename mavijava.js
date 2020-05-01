2eacef8ed12be7c171cadda73dd2be4b317fb9f3
aliekm <51740366+aliekm@users.noreply.github.com>
2020-05-01 13:03:53 +0300

    Add files via upload

--- mavijava.js
+++ mavijava.js
@@ -17,7 +17,7 @@ if (!('remove' in Element.prototype)) {
   }
 
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoibWF2aWhhcml0YW0iLCJhIjoiY2s5bnZqamQwMDJweDNsbjZncWZsM3h6cSJ9.wWaNmt1YUbF7rXhHkxVw4w';  
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWF2aWhhcml0YW0iLCJhIjoiY2s5aW4zYmM2MDBhODNmc2FjZzRqdzRyYSJ9.I27OU2_08z8wvRI_HwpPPA';  
     var map = new mapboxgl.Map({
         container: 'map', // container id
         style: 'mapbox://styles/maviharitam/ck9mutru61trg1inrk5ntrk7f',
@@ -71,10 +71,14 @@ function setPriceImage() {
 
 function setPumpImage() {
   var pumpImage = document.createElement("img");
-  if (feature.properties.pump === "2" || feature.properties.pump === "1") {
-    pumpImage.src = "pump1.svg";  
-  } else {
-    pumpImage.src = "pump0.svg"
+  if (feature.properties.pump === "1") {
+    pumpImage.src = "icon_1.svg";  
+  }
+  else if (feature.properties.pump === "") {
+    pumpImage.src = "icon_2.svg";  
+  }
+  else {
+    pumpImage.src = "icon_0.svg"
   }
   var vacuum = document.getElementById("pumpID").appendChild(pumpImage);
   vacuum.className = "pumpImg";
@@ -84,7 +88,11 @@ function setWaterImage() {
   var waterImage = document.createElement("img");
   if (feature.properties.water === "1") {
     waterImage.src = "water1.svg";  
-  } else {
+  }
+  else if (feature.properties.water === "") {
+    waterImage.src = "water2.svg";  
+  }
+  else {
     waterImage.src = "water0.svg"
   }
   var water = document.getElementById("waterAcID").appendChild(waterImage);
@@ -95,7 +103,11 @@ function setACImage() {
   var acImage = document.createElement("img");
   if (feature.properties.AC === "1") {
     acImage.src = "ac1.svg";  
-  } else {
+  }
+  else if (feature.properties.AC === "") {
+    acImage.src = "ac2.svg";  
+  }
+  else {
     acImage.src = "ac0.svg"
   }
   var ac = document.getElementById("waterAcID").appendChild(acImage);
@@ -166,36 +178,65 @@ function setVhf() {
   var latSeconds = Math.round(((feature.geometry.coordinates[1] - Math.floor(feature.geometry.coordinates[1]))*60 - latMinutes)*60);
 
 
-if ((feature.properties.pump == "1") || (feature.properties.pump == "2"))  {
-  var pumpFlag = "var"
-} else if (feature.properties.pump == 0) {
-  var pumpFlag = "yok"
+if ((feature.properties.pump === "1") || (feature.properties.pump === "2"))  {
+  var pumpFlag = "Vakum pompası var"
+} else if (feature.properties.pump === "0") {
+  var pumpFlag = "Vakum pompası yok"
+}
+else {
+  var pumpFlag = "Bilgi yok"
 }
 
-if (feature.properties.AC == 1) {
-  var acFlag = "var"
-} else if (feature.properties.AC == 0) {
-  var acFlag = "yok"
+if (feature.properties.AC === "1") {
+  var acFlag = "220V var"
+} else if (feature.properties.AC === "0") {
+  var acFlag = "220V yok"
+}
+else {
+  var acFlag = "Bilgi yok"
 }
 
-if (feature.properties.water == 1) {
-  var waterFlag = "var"
-} else if (feature.properties.water == 0) {
-  var waterFlag = "yok"
+if (feature.properties.water === "1") {
+  var waterFlag = "Su var"
+} else if (feature.properties.water === "0") {
+  var waterFlag = "Su yok"
+}
+else {
+  var waterFlag = "Bilgi yok"
 }
 
+if (feature.properties.price === "") {
+  var priceFlag = "Bilgi yok"
+}
+else {
+  var priceFlag = feature.properties.price + "TL/ton";
+}
+
+if (feature.properties.contacts === "") {
+  var contactsFlag = "Bilgi yok"
+}
+else {
+  var contactsFlag = feature.properties.contacts;
+}
+
+if (feature.properties.vhf === "") {
+  var vhfFlag = "Bilgi yok"
+}
+else {
+  var vhfFlag = feature.properties.vhf;
+}
 
 var popup = new mapboxgl.Popup({ offset: [0, -15] })
   .setLngLat(feature.geometry.coordinates)
   .setHTML('<h3 id = "facilityName">' + feature.properties.facility + '</h3><ul>' 
    // + '<li class = "popUpWriting" id = "highnessID">' + '<p id = "hignessWriting">' + 'Memnuniyet:  ' + feature.properties.highness + '/5' + '</p>' + '</li>' 
-    + '<li class = "popUpWriting popup_li" id = "priceID">' + '<p id = "priceWriting">' + feature.properties.price + ' TL/ton' + '</p>' + '</li>' 
-    + '<li class = "popup_li" id = "pumpID">' + '<p id = "pumpWriting">' + 'Vakum pompası ' + pumpFlag + '</p>' +  '</li>' + '<br>'
-    + '<li class = "popup_li" id = "waterAcID">' + '<p id = "waterAcWriting">' + 'Su ' + waterFlag + ', 220V ' + acFlag +'</p>' +  '</li>'
+    + '<li class = "popUpWriting popup_li" id = "priceID">' + '<p id = "priceWriting">' + priceFlag + '</p>' + '</li>' 
+    + '<li class = "popup_li" id = "pumpID">' + '<p id = "pumpWriting">' + pumpFlag + '</p>' +  '</li>' + '<br>'
+    + '<li class = "popup_li" id = "waterAcID">' + '<p id = "waterAcWriting">' + waterFlag + ', ' + acFlag +'</p>' +  '</li>'
     + '<div class = "spacer">' + '</div>'+ '<li class = "singleImg popup_li" id = "addressImgID"></li>' 
     + '<li class = "popup_li" id = "addressID">' + latDegrees + '°' + latMinutes + "'" + latSeconds + '"N '  + longDegrees + '°' + longMinutes + "'" + longSeconds + '"E' + '</li>' + '<br>' 
-    + '<li class = "singleImg popup_li" id = "contactsImgID"> </li>' + '<li class = "popup_li" id = "contactsID">' + feature.properties.contacts + '</li>' + '<br>' 
-    + '<li class = "singleImg popup_li" id = "vhfImgID"> </li>'+ '<li class = "popup_li" id = "vhfID">' + 'VHF: ' + feature.properties.vhf + '</li>'
+    + '<li class = "singleImg popup_li" id = "contactsImgID"> </li>' + '<li class = "popup_li" id = "contactsID">' + contactsFlag + '</li>' + '<br>' 
+    + '<li class = "singleImg popup_li" id = "vhfImgID"> </li>'+ '<li class = "popup_li" id = "vhfID">' + 'VHF: ' + vhfFlag + '</li>'
     + '<li class = "popup_li" id = "shareInfoID"> </li>'
     + '<li class="popup_li" id="shareInfoID" data-toggle="modal" data-target=".form">Bu işletme ile ilgili bilgi önerin</li>'
     + '</ul>'
